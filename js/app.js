49f90e987341b94863174b6abe1f42b475d5bfda
Franco Ruiz <francojruiz10@gmail.com>
2023-08-19 09:49:35 -0300

    pagina de login agregada

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const $submit=document.getElementById("submit"),
+    $username=document.getElementById("username"),
+    $password=document.getElementById("password"),
+    $visible=document.getElementById("visible");
+
+document.addEventListener("change",(e)=>{
+        if(e.target===$visible){
+            if($visible.checked===false)$password.type="password";
+            else $password.type="text"
+        }
+    })
+
+document.addEventListener("click", (e) =>{
+    if(e.target===$submit){
+        if($password.value !== "" && $username.value !== ""){
+            e.preventDefault();
+            window.location.href="home.html";
+        } 
+    }
+})
